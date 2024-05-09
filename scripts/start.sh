79a9cac8f6e814f67e21b49f32a0434f6effa29d
Paul Hamilton <phamtec@mac.com>
2024-05-09 17:25:50 +1000

    Setup lib paths.

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,4 +8,11 @@ then
   exit 1
 fi
 
+if [ -d zmqirc/lib ];
+then
+  # for those machines that have local libs on them
+  LD_LIBRARY_PATH=/home/visualops/zmqirc/lib
+  export LD_LIBRARY_PATH
+fi
+
 ./zmqirc/build/ZMQIRC > zmqirc.log 2>&1 &
