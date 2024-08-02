1b25d3e48e7582fcfbfecb5145a60fd108a03a6f
Paul Hamilton <phamtec@mac.com>
2024-08-02 16:44:42 +1000

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 # 
 # Stop Nodes IRC.
 
-LIST=`ps -ef | grep "nodes-irc" | grep -v grep | awk '{print $2}'`
+LIST=`ps -ef | grep "build/nodes-irc" | grep -v grep | awk '{print $2}'`
 if [ ! -z "$LIST" ]; then
 	sudo kill -9 $LIST
 fi
